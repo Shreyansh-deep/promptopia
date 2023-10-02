448708607176f5b8d5dbe0dc0aa1803f95e73178
Shreyansh-deep <84981057+Shreyansh-deep@users.noreply.github.com>
2023-10-02 13:30:23 +0530

    Added search feature

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import PromptCard from './PromptCard'
 
 const PromptCardList = ({ data, handleTagClick }) => {
@@ -20,38 +20,84 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState('')
   const [posts, setPosts] = useState([])
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchedResults, setSearchedResults] = useState([])
+  //const inputRef = useRef()
+  const fetchPosts = async () => {
+    const response = await fetch('/api/prompt')
+    const data = await response.json()
 
-  const handleSearchChange = (e) => {
-
+    setPosts(data)
   }
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch('/api/prompt')
-      const data = await response.json()
-
-      setPosts(data)
-    }
-     fetchPosts();
+    fetchPosts();
   }, [])
+
+  const filterPrompts  = (searchText) => {
+    const regex = new RegExp(searchText, "i")
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    )
+  }
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
+  
 
   return (
     <section className="feed">
-      <form className="relative w-full flex-center"> 
+      <form 
+        className="relative w-full flex-center"
+        // onSubmit={(e) => {
+        // e.preventDefault()
+        // console.log(inputRef.current.value)
+        // }}
+      > 
         <input
           type="text"
           placeholder="Search for a tag or a username"
           value={searchText}
+          //ref={inputRef}
           onChange={handleSearchChange}
           required
           className="search_input peer"
         />
       </form>
 
-      <PromptCardList 
+      {searchText ? (
+        <PromptCardList 
+        data={searchedResults}
+        handleTagClick = {handleTagClick}
+        />
+      ) :(
+        <PromptCardList 
         data={posts}
-        handleTagClick = {() => {}}
-      />
+        handleTagClick = {handleTagClick}
+        />
+      )}
+      
     </section>
   )
 }
